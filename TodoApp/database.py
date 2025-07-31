ee2863ba3a68489a6b307982b236dd9cb5a7d01b
Arslan Ovezov <arslan.datascience@gmail.com>
2025-07-31 19:41:32 +0500

    ars3

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,3 +1,7 @@
 from sqlalchemy import create_engine 
+from sqlalchemy.orm import sessionmaker
 
- 
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todo.db" # Using SQLite for simplicity
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
