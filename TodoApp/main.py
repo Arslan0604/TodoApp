5d45b500defb23e3b3bad44fabd31b494cf951d5
Arslan Ovezov <arslan.datascience@gmail.com>
2025-08-03 19:41:32 +0500

    ars11

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, Optional
 from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends, HTTPException, Path, status
@@ -57,7 +57,7 @@ async def update_todo(db: db_dependency,
                       todo_id: int, 
                       todo_request: TodoRequest):
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
-    if todo_model is not None:
+    if todo_model is None:
         raise HTTPException(status_code=404, detail='Todo not found.')
     
     todo_model.title = todo_request.title
