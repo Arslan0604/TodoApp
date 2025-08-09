49f21c368add0b1a1b70edbd2de8a0229e5d0916
Arslan Ovezov <arslan.datascience@gmail.com>
2025-08-09 20:01:56 +0500

    ars9

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -14,7 +14,15 @@ class CreateUserRequest(BaseModel):
 
 @router.post("/auth/")
 async def create_user(create_user_request: CreateUserRequest):
-    create_user_model = Users(**create_user_request.model_dump())
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name =create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=create_user_request.password,
+        is_active=True
+    )
     
     
     
