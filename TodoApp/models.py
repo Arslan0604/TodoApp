1a0754213077e0cdc2635660f7cbdd96aab57029
Arslan Ovezov <arslan.datascience@gmail.com>
2025-08-09 16:33:19 +0500

    ars2

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,6 +1,19 @@
 from database import Base 
 from sqlalchemy import Column, Integer, String, Boolean
 
+
+class Users(Base):
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    urername = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    
+    
+    
 # create database model
 class Todos(Base):
     __tablename__ = 'todos'
