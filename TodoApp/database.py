5cab7fb75548f9b231daf9818335480000596718
Arslan Ovezov <arslan.datascience@gmail.com>
2025-11-30 12:17:14 +0500

    ars1

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost/TodoApplicationDatabase" # its for postgres
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:arslan060482!@localhost:3306/TodoApplicationDatabase" #its for mysql
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:arslan060482@localhost:3306/TodoApplicationDatabase" #its for mysql
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
