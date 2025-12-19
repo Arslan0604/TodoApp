bf591fb6c8bd7840b085c463d0fd0b35072a1621
Arslan Ovezov <arslan.datascience@gmail.com>
2025-12-19 13:46:39 +0500

    ars6

--- TodoApp/test/test_example.py
+++ TodoApp/test/test_example.py
@@ -2,3 +2,13 @@ def test_equal_or_not_equal():
     assert 3 == 3
     assert 3 != 1
     
+    
+def test_is_instance():
+    assert isinstance("this is a string", str)
+    assert not isinstance('10', int)
+    
+    
+def test_boolean():
+    validated = True
+    assert validated is True
+    assert ('hello' == 'world') is False
