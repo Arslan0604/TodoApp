7e6ce1fd837a34a2c13be4cb05aee8bd318ce222
Arslan Ovezov <arslan.datascience@gmail.com>
2025-12-19 13:56:06 +0500

    ars7

--- TodoApp/test/test_example.py
+++ TodoApp/test/test_example.py
@@ -12,3 +12,20 @@ def test_boolean():
     validated = True
     assert validated is True
     assert ('hello' == 'world') is False
+    
+def test_type():
+    assert type("hello" is str)
+    assert type('world' is not int)
+    
+    
+def test_greater_and_less_than():
+    assert 7 > 3
+    assert 4 < 10
+    
+def test_list():
+    num_list = [1, 2, 3, 4, 5]
+    any_list = [False, False]
+    assert 1 in num_list
+    assert 7 not in any_list
+    assert all(num_list)
+    assert not any(any_list)
