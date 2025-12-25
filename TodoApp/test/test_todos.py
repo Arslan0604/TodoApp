9be80430e6973989a2155864f13d0876e36e5f63
Arslan Ovezov <arslan.datascience@gmail.com>
2025-12-25 17:15:03 +0500

    ars10

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -58,4 +58,6 @@ def test_todo():
 def test_read_all_authenticated():
     response = client.get("/")
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() == []
+    assert response.json() == [{'complete': False, 'title': 'learn to code', 
+                                'description': 'Need to learn everyday!', 
+                                'owner_id': 1, 'priority': 5}]
