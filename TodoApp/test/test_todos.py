daada382ded1837da952d477b16fbba4a35cc90a
Arslan Ovezov <arslan.datascience@gmail.com>
2025-12-25 15:18:01 +0500

    ars6

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -3,6 +3,9 @@ from sqlalchemy.pool import StaticPool
 from ..database import Base
 from sqlalchemy.orm import sessionmaker
 from ..main import app
+from ..routers.todos import get_db, get_current_user
+from fastapi.testclient import TestClient
+from fastapi import status
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./testdb.db"
 
@@ -29,3 +32,11 @@ def override_get_current_user():
         
 app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
+
+client = TestClient(app)
+
+
+def test_read_all_authenticated():
+    response = client.get("/")
+    assert response.status_code == status.HTTP_200_OK
+    
