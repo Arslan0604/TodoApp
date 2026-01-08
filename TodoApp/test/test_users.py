d4c98cb79443f7c07e6b139ad58919ed6cddb146
Arslan Ovezov <arslan.datascience@gmail.com>
2026-01-08 14:52:52 +0500

    ars4

---
+++ TodoApp/test/test_users.py
@@ -0,0 +1,12 @@
+from .utils import *
+from ..routers.users import get_db, get_current_user
+from fastapi import status
+
+
+app.dependency_overrides[get_db] = override_get_db
+app.dependency_overrides[get_current_user] = override_get_current_user
+
+def test_get_user():
+    response = client.get('/user/')
+    assert response.status_code == status.HTTP_200_OK
+    
