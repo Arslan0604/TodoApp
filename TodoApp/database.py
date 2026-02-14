e47ca0dc21ed1c5a02d61a332d6e8846e69c2629
Arslan Ovezov <arslan.datascience@gmail.com>
2026-02-14 19:44:28 +0500

    ars1

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine 
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os   # this I have added to fix
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost/TodoApplicationDatabase" # its for postgres
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:arslan060482@127.0.0.1:3306/TodoApplicationDatabase" #its for mysql
+# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:arslan060482@127.0.0.1:3306/TodoApplicationDatabase" #its for mysql
+SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URL") # this I have added last
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
