58f8a544e2532e49c40fa0f93b17f3a2b1a907bc
Arslan Ovezov <arslan.datascience@gmail.com>
2026-02-15 22:11:11 +0500

    ars4

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -5,8 +5,8 @@ import os   # this I have added to fix
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost/TodoApplicationDatabase" # its for postgres
 # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:arslan060482@127.0.0.1:3306/TodoApplicationDatabase" #its for mysql
-# SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URL") # this I have added last
-SQLALCHEMY_DATABASE_URL = "postgresql://todo_postgresql_y4y6_user:kju4dELbNR5BQQXVTM4oIZGpL6l6SLFK@dpg-d688mhogjchc73beu4pg-a.oregon-postgres.render.com/todo_postgresql_y4y6"
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL") # this I have added last
+# DATABASE_URL = "postgresql://todo_postgresql_y4y6_user:kju4dELbNR5BQQXVTM4oIZGpL6l6SLFK@dpg-d688mhogjchc73beu4pg-a.oregon-postgres.render.com/todo_postgresql_y4y6"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
@@ -16,5 +16,3 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
-"postgresql://todo_postgresql_y4y6_user:kju4dELbNR5BQQXVTM4oIZGpL6l6SLFK@dpg-d688mhogjchc73beu4pg-a.oregon-postgres.render.com/todo_postgresql_y4y6"
-# postgresql://todo_postgresql_y4y6_user:kju4dELbNR5BQQXVTM4oIZGpL6l6SLFK@dpg-d688mhogjchc73beu4pg-a.oregon-postgres.render.com/todo_postgresql_y4y6
