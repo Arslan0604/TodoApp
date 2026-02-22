50c5cb966041f1cf1d959772337f22d3d2f9db93
Arslan Ovezov <arslan.datascience@gmail.com>
2026-02-22 17:49:37 +0500

    sql basa doesnot work

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -15,4 +15,27 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+# import os
 
+# SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
+
+# # 🔥 если запускаем локально и переменной нет
+# if not SQLALCHEMY_DATABASE_URL:
+#     SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+
+# # 🔥 фикс для Render (если начинается с postgres://)
+# if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+#     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace(
+#         "postgres://", "postgresql://", 1
+#     )
+
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL,
+#     connect_args={"check_same_thread": False} if "sqlite" in SQLALCHEMY_DATABASE_URL else {}
+# )
+
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base = declarative_base()
